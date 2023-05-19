a33c3b256c0fc5816fe4e82a2616702fd3e9567c
Mennatallah <menatallahzaid@gmail.com>
2023-05-20 02:31:10 +0300

    Build My World project

---
+++ script/welcome.cpp
@@ -0,0 +1,18 @@
+//print heelo world as the simulation starts
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin // creating plugin inherts from gazebo worldplugin class
+  {
+    public: WorldPluginMyRobot() : WorldPlugin() // the constractor of the gazebo class
+            {
+              printf("Welcome to Menna’s World!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) //load function (mandatory) receives info from thwe world file
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot) //register the plugin with the simulator
+}
